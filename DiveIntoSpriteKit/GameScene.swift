bedb6e0fb78182309f7359bd4a4f70e246b9942e
Ty Newton <ty-newton@users.noreply.github.com>
2022-04-29 12:40:49 +1000

    Initial Commit

---
+++ DiveIntoSpriteKit/GameScene.swift
@@ -0,0 +1,29 @@
+//
+//  GameScene.swift
+//  DiveIntoSpriteKit
+//
+//  Created by Paul Hudson on 16/10/2017.
+//  Copyright © 2017 Paul Hudson. All rights reserved.
+//
+
+import SpriteKit
+
+@objcMembers
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        // this method is called when your game scene is ready to run
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // this method is called when the user touches the screen
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // this method is called when the user stops touching the screen
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        // this method is called before each frame is rendered
+    }
+}
+
